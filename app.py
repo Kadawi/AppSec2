2dca29cea4ca67afd894577da289d54c03ebfe54
Kaitlyn Wimberley <kdw325@nyu.edu>
2019-10-10 21:26:00 -0400

    Started building out app.py
    Started adding session management, nested dictionary, log-in logic, messaging, etc.
    Added flaskenv file to run in developer mode for debugging
    Added login.html to templates
    Also added a few extra html templates from flask tutorial - rm later

--- app.py
+++ app.py
@@ -1,25 +1,85 @@
 
-m flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for, request, session, flash, g
+
+from functools import wraps
+
 app = Flask(__name__)
 
+Users = {}
+
+def checkUser(dict, key):
+    if key in dict.keys():
+        return false
+    else:
+        return true
+
+#create new decorator to require login
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        #adjust later for more secure sessionID
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+#secret key for establishing sessions
+app.secret_key = "replaceAndMoveLater"
+
 @app.route('/')
-def something():
-    something
+def home():
+    ##return "Hello, world!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+#@app.route('/register', methods=['POST', 'GET'])
+#def register():
+#    if request.method == 'POST':
+#        user = request.form['uname']
+#        pwd = request.form['pword']
+#        2fa = request.form['2fa']
+        if checkKey(Users, user)
+            Users{user: {'pass': pwd, '2fa': 2fa}}
+        else flash('Failure')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    error = None
     if request.method == 'POST':
-        user = request.form['username']
-        pwd = request.form['password']
-        other stuff
-    if request.method == 'GET':
-        #serve them the registration page
+        #verify login credentials
+        if request.form['uname'] != 'admin' or request.form['pword'] != 'admin':
+            #return failure if incorrect credentials
+            error = 'failure'
+        else:
+            #Set sessionID on success
+            session['logged_in'] = True
+            #add success message here
+            flash('Success')
+            #redirect to spell_check
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+    
 
-@app.route('/login', methods['POST', 'GET'])
+#@app.route('/spell_check', methods=['POST', 'GET'])
+#@login_required
 
-@app.route('/spell_check', methods=['POST', 'GET'])
+#route for logging out
+#@app.route('/logout')
+#@login_required
+#def logout():
+    #remove sessionID on logout
+#    session.pop('logged_in', None)
+#    flash('logged out')
+    #redirect to home
+#return redirect(url_for('login'))
 
 
